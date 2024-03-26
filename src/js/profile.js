9485d12139aa73360f236c32b7213a2841a719a7
Sadegh Sargeran <sadegh.sg.sn@gmail.com>
2024-03-26 16:27:13 +0330

    Insurance details initiation added

--- src/js/profile.js
+++ src/js/profile.js
@@ -2,32 +2,47 @@ import {axiosAgent, CreateNavSide, NotificationModal} from "./utils.js";
 import $ from './jquery.module.js';
 import './axios.min.js';
 
-CreateNavSide('profile.html')
+function InitiateProfile() {
+    axiosAgent.get('/profile/')
+        .then((response) => {
+            let profile = response.data
+            let insurance_details = profile.insurance_details
+            let access_level_data = profile.access_level_data
+            delete profile.insurance_details
+            delete profile.access_level_data
+            delete profile.id
+            delete profile.account_state
 
-axiosAgent.get('/profile/')
-    .then((response) => {
-        let profile = response.data
-        let insurance_details = profile.insurance_details
-        let access_level_data = profile.access_level_data
-        delete profile.insurance_details
-        delete profile.access_level_data
-        delete profile.id
-        delete profile.account_state
+            // Profile initiation
+            $.each(profile, function (field, value) {
+                $(`#${field}-input`).val(value)
+            })
 
-        // Profile initiation
-        $.each(profile, function(field, value) {
-            $(`#${field}-input`).val(value)
+            // Access_level_data initiation
+            $("#access-level").text(`"${access_level_data.access_level}"`)
+            $("#branch").text(access_level_data.branch ? `"${access_level_data.branch}"` : "----")
+            $("#title").text(access_level_data.title ? `"${access_level_data.title}"` : "----")
+
+            // Insurance initiation
+            $.each(insurance_details, function(field, value) {
+                if (value == null)
+                    $(`#${field}`).text("----")
+                else if (value)
+                    $(`#${field}`).text('"دارد"')
+                else
+                    $(`#${field}`).text('"ندارد"')
+            })
         })
+        .catch((error) => {
+            console.error(error)
+            NotificationModal("error", "خطا در واکشی پروفایل", `status:${error.response.status}, message:${error.response.data.detail}`)
+        })
+}
 
-        // Access_level_data initiation
-        $("#access-level").text(access_level_data.access_level)
-        $("#branch").text(access_level_data.branch ? access_level_data.branch : "----")
-        $("#title").text(access_level_data.title ? access_level_data.title : "----")
+function addEventListener() {
 
-        // Insurance initiation
+}
 
-    })
-    .catch((error) => {
-        console.error(error)
-        NotificationModal("error", "خطا در واکشی پروفایل", `status:${error.response.status}, message:${error.response.data.detail}`)
-    })
+CreateNavSide('profile.html')
+InitiateProfile();
+addEventListener();
